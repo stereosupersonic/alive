a68d2dcfa70388cb273efa393aac92d894617f39
Michael Deimel <michael@deimel.de>
2012-05-08 18:04:00 +0200

    some customization

--- app/controllers/heartbeats_controller.rb
+++ app/controllers/heartbeats_controller.rb
@@ -2,10 +2,8 @@ class HeartbeatsController < ApplicationController
   before_filter :authenticate
 
   def create
-    @client = Client.find_or_create_by_mac params[:client][:mac]
-
-    @client.attributes =  params[:client]
-
+    @client = Client.find_or_create_by_mac params[:client][:mac]     
+    @client.attributes =  params[:client].merge(:heartbeated_at => Time.now)
     respond_to do |format|
       if @client.save 
         format.json { render :json =>  @client, :status => 200, :location => @client }
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: clients
+#
+#  id             :integer         not null, primary key
+#  name           :string(255)
+#  ip             :string(255)
+#  mac            :string(255)
+#  comment        :text
+#  heartbeated_at :datetime
+#  created_at     :datetime        not null
+#  updated_at     :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Client do
