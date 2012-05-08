645e79c1dd0ebc8ca07bff291fb2b7a6535c3c34
Michael Deimel <michael@deimel.de>
2012-05-08 18:26:34 +0200

    add song text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,6 @@ module ApplicationHelper
   def logged_in?
     not request.authorization.nil?
   end
+  
+
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,43 @@
-module HomeHelper
+module HomeHelper    
+  
+  def alive
+    %{
+      "Son," she said, "Have I got a little story for you. 
+      What you thought was your daddy was nothing but a... 
+      While you were sitting home alone at age thirteen your real daddy was dying. 
+      Sorry you didn't see him but I'm glad we talked."
+
+      Oh, I... Oh, I'm still alive. 
+      Hey, I... Oh, I'm still alive. 
+      Hey, I... Oh, I'm still alive. 
+      Hey, ooh.
+
+      Oh, she walks slowly across a young man's room. 
+      She said, "I'm ready for you." 
+      Why I can't remember anything to this very day? 
+      'Cept the look. The look. 
+      Oh, you know where. Now I can't see I just stare.
+
+      I... I'm still alive. 
+      Hey I... But, I'm still alive. 
+      Hey I... Boy, I'm still alive. 
+      Hey I... Oh, I'm still alive. Yeah. 
+      Ooh yeah. Yeah yeah yeah. Oh. Ooh.
+
+      "Is something wrong?" She said. Of course there is. 
+      "You're still alive." She said. Oh, and do I deserve to be? 
+      Is that the question? And if so, if so, who answers? Who answers!
+
+      I... Oh, I'm still alive. 
+      Hey I... Oh, I'm still alive. 
+      Hey I... But, I'm still alive. 
+      Yeah I... Ooh, I'm still alive. 
+      Yeah, yeah, yeah, yeah, yeah, yeah.
+
+      Woh uh huhu. Yeah uh huhu. Ohh woh uh huhu. Yeah uh huhu. Yeah, yeah, yeah, yeah.
+      
+      Pearl Jam - Ten
+      Songwriters: GOSSARD, STONE C. / VEDDER, EDDIE JEROME
+    }
+   end
 end
